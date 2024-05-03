14f6e6cb1e8b26d91b01c31949a631d5e24fc2d2
lé <elcasaboxi@gmail.com>
2024-05-03 18:22:44 +0900

    config warning twix

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
+	onwarn: (warning, handler) => {
+		if (warning.code === 'css-unused-selector') return;
+		if (warning.code === "a11y-click-events-have-key-events") return;
+		if (warning.code === "a11y-no-static-element-interactions") return;
+		handler(warning);
+	},
+	
 	kit: {
 		adapter: adapter()
 
